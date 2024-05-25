80df828689791c6badeafa50f2a37f6f4c239fa8
Cnidarias <roessner.pascal@gmail.com>
2024-05-25 15:11:06 +0200

    🔥 Google: Remove all google dependencies because of Cookies
    This allows us to avoid the entire coolie policy banner that's really
    super annoying. Maybe we'll have to add it again at some point if we
    want to have fancier features again

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-function initMap() {
-		const fab_loc = { lat: 50.38028649499958, lng: 7.54943587324097}
-		const map = new google.maps.Map(document.getElementById("map"), {
-				zoom: 18,
-				center: fab_loc,
-		});
-		const marker = new google.maps.Marker({
-				position: fab_loc,
-				map: map,
-		});
-}
-
 function closeTopNav() {
 		const x = document.getElementById("topNav");
 		x.className = "topnav";
